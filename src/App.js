f1517670b34583ee9897a2155344ea9c2a046cdc
Aneesa Kunju <aneesakunju@yahoo.com>
2024-06-21 02:04:03 +0100

    Deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline, createTheme } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import styles from './App.module.css';
 
 /*
 const theme = createTheme({
@@ -28,7 +29,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
-      <div>
+      <div className={styles.container}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,21 @@
 
+import GithubRepoTable from "../../components/github-repo-table/GithubRepoTable";
 import styles from "./Home.module.css";
 
 function Home() {
     return (
         <div className={styles.container}>
-            Home Page
+                <h1>Aneesa Kunju</h1>
+                <p>Software Engineer</p>
+            <p>
+                <a href="https://www.linkedin.com/in/aneesa-k-7309813/">Aneesa Kunju LinkedIn</a>
+            </p>
+            <h3>
+                Projects
+            </h3>
+            <div className={styles.table}>
+                <GithubRepoTable />
+            </div>
         </div>
     );
 }
