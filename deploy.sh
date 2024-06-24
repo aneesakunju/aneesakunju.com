88a1d4a93d6ecb5655ede651e7a18f9f1387042a
Aneesa Kunju <aneesakunju@yahoo.com>
2024-06-24 16:20:29 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 npm install && \
 rm -rf build && \
 rm -rf docs && \
+# github serves up the docs folder, so moving build to docs below.
 npm run build && mv build docs && \
 echo -n "aneesakunju.com" > docs/CNAME && \
 git add . && git commit -m "deploy" && \
