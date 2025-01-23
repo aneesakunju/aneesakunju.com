a48f63c8322b755ee83606f53494d85189b4f3e1
Aneesa Kunju <aneesakunju@yahoo.com>
2025-01-23 02:31:02 +0000

    deploy

--- src/components/github-repo-table/GithubRepoTable.js
+++ src/components/github-repo-table/GithubRepoTable.js
@@ -84,6 +84,7 @@ function createData(name, url) {
 }
 
 const rows = [
+  createData('Spring Boot Microservices with Eureka', 'https://github.com/aneesakunju/microservices'),
   createData('Foreign Exchange Rates APIs - Kotlin', 'https://github.com/aneesakunju/FXRateAPIs'),
   createData('Simple Load Balancer - Java', 'https://github.com/aneesakunju/load-balancer'),
 ].sort((a, b) => (a.name < b.name ? -1 : 1));
